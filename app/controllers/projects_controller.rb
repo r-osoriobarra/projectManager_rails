f1da210a7cce65bc29e0e5014d6124c072275d0d
rodrigo <rob.developer@protonmail.com>
2021-07-25 16:13:35 -0400

    add: validations, errors view and dashboard table

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,8 +3,18 @@ class ProjectsController < ApplicationController
   end
 
   def create
+    @project = Project.create(name: params[:name], 
+                              description: params[:description],
+                              start_date: params[:start_date],
+                              end_date: params[:end_date],
+                              state: params[:state])
   end
 
   def dashboard
+    if params[:state].present?
+      @projects = Project.where('state = ?', params[:state])
+    else
+      @projects = Project.all
+    end
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,3 @@
 class Project < ApplicationRecord
+    validates :name, :description, :start_date, presence: true
 end
